e70f636438b9855d36720997eaad9b15c2388c20
Thang Tran <thangtran@brodev.com>
2022-06-17 14:44:02 +0700

    update deploy function

--- src/deploy.rs
+++ src/deploy.rs
@@ -1,18 +1,17 @@
 use crate::*;
 
 const INITIAL_BALANCE: Balance = 3_000_000_000_000_000_000_000_000; // 3e24yN, 3N
-const CODE: &[u8] = include_bytes!("../out/p2p_loan.wasm");
 
 #[near_bindgen]
 impl Contract {
     #[private]
-    pub fn create_child_contract(prefix: AccountId) -> Promise {
+    pub fn create_child_contract(prefix: AccountId, code: Vec<u8>) -> Promise {
         let subaccount_id =
             AccountId::try_from(format!("{}.{}", prefix, env::current_account_id()));
         Promise::new(subaccount_id.unwrap())
             .create_account()
             .add_full_access_key(env::signer_account_pk())
             .transfer(INITIAL_BALANCE)
-            .deploy_contract(CODE.to_vec())
+            .deploy_contract(code.to_vec())
     }
 }
